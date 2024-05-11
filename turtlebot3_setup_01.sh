69e0ca31dd1b9e8aa9024323922780246010454a
yuki9854 <72436563+yuki9854@users.noreply.github.com>
2024-05-11 18:43:27 +0900

    Create turtlebot3_setup_01.sh

---
+++ turtlebot3_setup_01.sh
@@ -0,0 +1,5 @@
+sudo apt install python3-argcomplete python3-colcon-common-extensions libboost-system-dev build-essential -y
+sudo apt install ros-humble-hls-lfcd-lds-driver -y
+sudo apt install ros-humble-turtlebot3-msgs -y
+sudo apt install ros-humble-dynamixel-sdk -y
+sudo apt install libudev-dev -y
